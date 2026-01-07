16308a355d130079b367913ac7d3758ff6bee773
Delight007 <ganalafiyalevi@gmail.com>
2026-01-07 23:27:59 +0100

    still working oh

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -3,9 +3,8 @@ import GlobalProvider from "./Components/context/GlobalContext.jsx";
 import Footer from "./Components/Footer/Footer.jsx";
 import Hero from "./Components/Hero/Hero.jsx";
 import Nav from "./Components/Navbar/Nav.jsx";
-import Skills from "./Components/Skills/Skills.jsx";
+import Skills from "./Components/skill/Skills.jsx";
 import WorkExperience from "./Components/WorkExperience/WorkExperience.jsx";
-
 function App() {
   return (
     <GlobalProvider>
